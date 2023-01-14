c61f7f14422ce2926e1809ae7d731ce0b8519320
Petrus Pierre <petrus.tsx@gmail.com>
2023-01-14 15:10:12 -0300

    complete order repository

--- src/infra/repository/order.repository.spec.ts
+++ src/infra/repository/order.repository.spec.ts
@@ -82,4 +82,132 @@ describe('Order repository test', () => {
       ],
     })
   })
+
+  it('should update an order', async () => {
+    const customerRepository = new CustomerRepository()
+    const customer = new Customer('123', 'Customer 1')
+    const address = new Address('St 1', 1, 'ZIP', 'City')
+    customer.changeAddress(address)
+    await customerRepository.create(customer)
+
+    const productRepository = new ProductRepository()
+    const product = new Product('123', 'Product 1', 10)
+    await productRepository.create(product)
+
+    const orderItem = new OrderItem(
+      '1',
+      product.name,
+      product.price,
+      2,
+      product.id
+    )
+    const order = new Order('123', customer.id, [orderItem])
+
+    const orderRepository = new OrderRepository()
+    await orderRepository.create(order)
+
+    const newProduct = new Product('1234', 'Product 2', 20)
+    await productRepository.create(newProduct)
+    const newOrderItem = new OrderItem(
+      '2',
+      newProduct.name,
+      newProduct.price,
+      3,
+      newProduct.id
+    )
+
+    const newOrder = new Order('123', customer.id, [orderItem, newOrderItem])
+    await orderRepository.update(newOrder)
+
+    const orderModel = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ['items'],
+    })
+
+    expect(orderModel.toJSON()).toStrictEqual({
+      id: order.id,
+      customer_id: customer.id,
+      total: newOrder.total(),
+      items: [
+        {
+          id: orderItem.id,
+          name: orderItem.name,
+          price: orderItem.price,
+          quantity: orderItem.quantity,
+          order_id: order.id,
+          product_id: product.id,
+        },
+        {
+          id: newOrderItem.id,
+          name: newOrderItem.name,
+          price: newOrderItem.price,
+          quantity: newOrderItem.quantity,
+          order_id: order.id,
+          product_id: newProduct.id,
+        },
+      ],
+    })
+  })
+
+  it('should find order by id', async () => {
+    const customerRepository = new CustomerRepository()
+    const customer = new Customer('123', 'Customer 1')
+    const address = new Address('St 1', 1, 'ZIP', 'City')
+    customer.changeAddress(address)
+    await customerRepository.create(customer)
+
+    const productRepository = new ProductRepository()
+    const product = new Product('123', 'Product 1', 10)
+    await productRepository.create(product)
+
+    const orderItem = new OrderItem(
+      '1',
+      product.name,
+      product.price,
+      2,
+      product.id
+    )
+
+    const order = new Order('123', customer.id, [orderItem])
+    const orderRepository = new OrderRepository()
+    await orderRepository.create(order)
+
+    const foundOrder = await orderRepository.find(order.id)
+
+    expect(foundOrder).toStrictEqual(order)
+  })
+
+  it('should find all orders', async () => {
+    const customerRepository = new CustomerRepository()
+    const customer = new Customer('123', 'Customer 1')
+    const address = new Address('St 1', 1, 'ZIP', 'City')
+    customer.changeAddress(address)
+    await customerRepository.create(customer)
+
+    const productRepository = new ProductRepository()
+    const product = new Product('123', 'Product 1', 10)
+    await productRepository.create(product)
+
+    const orderItem = new OrderItem(
+      '1',
+      product.name,
+      product.price,
+      2,
+      product.id
+    )
+
+    const order = new Order('123', customer.id, [orderItem])
+    const orderRepository = new OrderRepository()
+    await orderRepository.create(order)
+
+    const foundOrders = await orderRepository.findAll()
+
+    expect(foundOrders).toStrictEqual([order])
+  })
+
+  it('should throw error when order is not found', async () => {
+    const orderRepository = new OrderRepository()
+
+    await expect(orderRepository.find('123')).rejects.toThrow('Order not found')
+  })
 })
--- src/infra/repository/order.repository.ts
+++ src/infra/repository/order.repository.ts
@@ -2,6 +2,7 @@ import Order from '../../domain/entity/order'
 import OrderRepositoryInterface from '../../domain/repository/order-repository.interface'
 import OrderModel from '../db/sequelize/model/order.model'
 import OrderItemModel from '../db/sequelize/model/order-item.model'
+import OrderItem from '../../domain/entity/order_item'
 
 export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
@@ -25,14 +26,83 @@ export default class OrderRepository implements OrderRepositoryInterface {
   }
 
   async update(entity: Order): Promise<void> {
-    throw new Error('Method not implemented.')
+    const sequelize = OrderModel.sequelize
+
+    await sequelize.transaction(async t => {
+      await OrderItemModel.destroy({
+        where: { order_id: entity.id },
+        transaction: t,
+      })
+      const items = entity.items.map(item => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        product_id: item.productId,
+        quantity: item.quantity,
+        order_id: entity.id,
+      }))
+      await OrderItemModel.bulkCreate(items, { transaction: t })
+      await OrderModel.update(
+        { total: entity.total() },
+        { where: { id: entity.id }, transaction: t }
+      )
+    })
   }
 
   async find(id: string): Promise<Order> {
-    throw new Error('Method not implemented.')
+    let orderModel
+
+    try {
+      orderModel = await OrderModel.findOne({
+        where: {
+          id: id,
+        },
+        rejectOnEmpty: true,
+        include: [{ model: OrderItemModel }],
+      })
+    } catch (err) {
+      throw new Error('Order not found')
+    }
+
+    const order = new Order(
+      orderModel.id,
+      orderModel.customer_id,
+      orderModel.items.map(
+        item =>
+          new OrderItem(
+            item.id,
+            item.name,
+            item.price,
+            item.quantity,
+            item.product_id
+          )
+      )
+    )
+
+    return order
   }
 
   async findAll(): Promise<Order[]> {
-    throw new Error('Method not implemented.')
+    const orderModels = await OrderModel.findAll({
+      include: [{ model: OrderItemModel }],
+    })
+
+    return orderModels.map(
+      orderModel =>
+        new Order(
+          orderModel.id,
+          orderModel.customer_id,
+          orderModel.items.map(
+            item =>
+              new OrderItem(
+                item.id,
+                item.name,
+                item.price,
+                item.quantity,
+                item.product_id
+              )
+          )
+        )
+    )
   }
 }
