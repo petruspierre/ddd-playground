b036745fa819f082e82b12a1f9ea410be733d38f
Petrus Pierre <petrus.tsx@gmail.com>
2023-01-16 17:04:05 -0300

    add support for domain events

---
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -0,0 +1,43 @@
+import EventDispatcherInterface from './event-dispatcher.interface'
+import EventHandlerInterface from './event-handler.interface'
+import EventInterface from './event.interface'
+
+export default class EventDispatcher implements EventDispatcherInterface {
+  private eventHandlers: {
+    [eventName: string]: EventHandlerInterface[]
+  } = {}
+
+  get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+    return this.eventHandlers
+  }
+
+  notify(event: EventInterface): void {
+    const eventName = event.constructor.name
+
+    if (this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName].forEach(handler => handler.handle(event))
+    }
+  }
+
+  register(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = []
+    }
+
+    this.eventHandlers[eventName].push(eventHandler)
+  }
+
+  unregister(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (!this.eventHandlers[eventName]) {
+      return
+    }
+
+    this.eventHandlers[eventName] = this.eventHandlers[eventName].filter(
+      handler => handler !== eventHandler
+    )
+  }
+
+  unregisterAll(): void {
+    this.eventHandlers = {}
+  }
+}
